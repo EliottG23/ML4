3964467365d8a0871317d818787a51cde5a5e4ca
EliottG23 <65028464+EliottG23@users.noreply.github.com>
2023-02-28 22:00:40 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from flask.json import jsonify
+from keras.models import load_model
+import ast
+import numpy as np
+
+app = Flask(__name__)
+
+# Model ML
+
+loaded_model = load_model('./model/fashion_mnist_model.h5')
+
+class_labels = {0: 'T-shirt/top',
+                1: 'Trouser',
+                2: 'Pullover',
+                3: 'Dress',
+                4: 'Coat',
+                5: 'Sandal',
+                6: 'Shirt',
+                7: 'Sneaker',
+                8: 'Bag',
+                9: 'Ankle boot'}
+
+# Routes
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route("/api/hello")
+def hello():
+    return jsonify({"hello": "world"})
+
+
+@app.route('/classify/<path:array>', methods=['GET', 'POST'])
+def classify(array):
+    image = ast.literal_eval(array)
+
+
+    image = np.array(image, dtype=np.float32)
+    image = image.reshape(1, 28, 28, 1)
+    image = image.astype("float32")
+    image = image / 255
+
+    prediction = loaded_model.predict(image)
+    label = int(np.argmax(prediction))
+    return jsonify({"prediction": label, 'label': class_labels[label]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
